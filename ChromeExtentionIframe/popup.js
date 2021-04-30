952ee6d6611fd0e6fb646ce50a0b2151b7278d2d
DESKTOP-O16UD8S\Carter Liu <gtk.carter.liu@gmail.com>
2021-04-30 22:04:48 +0800

    1. add a <div> for adding the list of frames. 2. adding a list of frames. 3. add a onload()

--- ChromeExtentionIframe/popup.js
+++ ChromeExtentionIframe/popup.js
@@ -1,12 +1,15 @@
 // Initialize butotn with users's prefered color
 let changeColor = document.getElementById("changeColor");
+let frameListDiv = document.getElementById("frameListDiv");
 
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+changeColor.addEventListener("click", readIframeAndChangeColorAsync);
+
+async function readIframeAndChangeColorAsync() {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
@@ -14,28 +17,46 @@ changeColor.addEventListener("click", async () => {
     function: setPageBackgroundColor,
   },
     (injectionResults) => {
+      console.log(injectionResults[0].result);
+      let innerList = "";
+      let buttonIdList = [];
+      if (injectionResults[0].result) {
+        let count = 1;
 
-      console.log(injectionResults);
+        for (const ele of injectionResults[0].result) {
+          let id = "iFrameDetector_Frame_" + count;
+          buttonIdList.push(id);
+          innerList += '<li><button id="' + id + '" >' + ele + "</button></li>"
+          count++;
+        }
+        frameListDiv.innerHTML = "<ol>" + innerList + "</ol>"
+      }
+      for (btnId of buttonIdList) {
+        btn = document.getElementById(btnId);
+        console.dir(btn);
+      }
     });
-});
+
+}
 
 // The body of this function will be execuetd as a content script inside the
 // current page
 function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    var iframe_element = document.getElementsByTagName("iframe");
-    var listOfUrl = []
-    for (i = 0; i < iframe_element.length; i++) {
-      iframe_element[i].style.backgroundColor = color;
-      iEle = iframe_element[i];
-      console.dir(iEle);
-      console.log(iEle.attributes.src.value)
-      listOfUrl.push(iEle.attributes.src.value)
-      //iEle.attributes.src.value = null;
-    }
-    console.log(listOfUrl)
-    return "listOfUrl";
+  var color = "#3aa757"
+  var iframe_element = document.getElementsByTagName("iframe");
+  var listOfUrl = []
+  for (i = 0; i < iframe_element.length; i++) {
+    iframe_element[i].style.backgroundColor = color;
+    iEle = iframe_element[i];
+    console.dir(iEle);
+    console.log(iEle.attributes.src.value)
+    listOfUrl.push(iEle.attributes.src.value)
+    //iEle.attributes.src.value = null;
+  }
+  console.log(listOfUrl)
 
-  });
-  return "listOfUrl"
+
+  return listOfUrl
 }
+
+window.onload = readIframeAndChangeColorAsync;
