9f6018f603dbd44e3c1da30886c992c02e34c9c6
DESKTOP-O16UD8S\Carter Liu <gtk.carter.liu@gmail.com>
2021-05-01 13:21:33 +0800

    not working version, keep a record: in popup.js line 105 tried closure line 66 tried tab.executeSript line 57 tried funtion eval to eval string to function

--- ChromeExtentionIframe/popup.js
+++ ChromeExtentionIframe/popup.js
@@ -32,12 +32,96 @@ async function readIframeAndChangeColorAsync() {
         frameListDiv.innerHTML = "<ol>" + innerList + "</ol>"
       }
       for (btnId of buttonIdList) {
-        btn = document.getElementById(btnId);
-        console.dir(btn);
+        let btn = document.getElementById(btnId);
+        console.log(btn.id)
+        btn.addEventListener("click", eventH);
       }
     });
+}
+
+async function eventH(event) {
+  console.log("########event\n", event);
+  console.dir(event)
+  let btn = event.target;
+  let id = parseInt(btn.id.substr(-1));
+  let domain = btn.innerText;
+  console.dir(btn);
+  console.log("id", id, "domain", domain);
+  await frameToggle(id, domain);
+}
+
+async function frameToggle(id, domain) {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  let obj = { id, domain };
+  console.log("var iframeDetectorVar = JSON.parse('" + JSON.stringify(obj) + "');")
+  var fn = Function("var iframeDetectorVar = JSON.parse('" + JSON.stringify(obj) + "');");
+  console.dir(chrome.tabs.executeScript)
+  chrome.storage.sync.set(obj, function () {
+    console.log('Value is set to ' + value);
+  });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: fn,
+  })
+  // chrome.tabs.executeScript(
+  //   tab.id,
+  //   {
+  //     code: "var iframeDetectorVar = JSON.parse('" + JSON.stringify(obj) + "');",
+  //   },
+  //   function () {
+  //     console.log("reached here");
+  //     chrome.scripting.executeScript({
+  //       target: { tabId: tab.id },
+  //       function: () => {
+  //         console.log(iframeDetectorVar);
+  //       }
+  //     })
+  //   }
+  // );
+}
+
+// async function toggleFrameSetting(id, domain) {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//   console.log(generateToggleFunc(id, domain))
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: func,
+//   })
+// }
+
+// function toggleFrameSrc(id, domain) {
+//   var iframe_element = document.getElementsByTagName("iframe");
+//   console.log(iframe_element)
+//   let targetIFrame = iframe_element[id - 1];
+//   let value = targetIFrame.attributes.src.value;
+//   if (value) {
+//     targetIFrame.attributes.src.value = null;
+//   }
+//   else {
+//     targetIFrame.attributes.src.value = domain;
+//   }
+// }
+
+function generateToggleFunc(id, domain) {
+  let _id = id;
+  let _domain = domain;
+  return function toggleFrameSrc() {
+    console.log("id:", id, "_id:", _id);
+    var iframe_element = document.getElementsByTagName("iframe");
+    console.log(iframe_element)
+    let targetIFrame = iframe_element[id - 1];
+    let value = targetIFrame.attributes.src.value;
+    if (value) {
+      targetIFrame.attributes.src.value = null;
+    }
+    else {
+      targetIFrame.attributes.src.value = domain;
+    }
+  }
 
 }
+
+
 
 // The body of this function will be execuetd as a content script inside the
 // current page
@@ -51,7 +135,6 @@ function setPageBackgroundColor() {
     console.dir(iEle);
     console.log(iEle.attributes.src.value)
     listOfUrl.push(iEle.attributes.src.value)
-    //iEle.attributes.src.value = null;
   }
   console.log(listOfUrl)
 
